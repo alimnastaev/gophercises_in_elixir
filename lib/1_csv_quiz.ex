16b35f2b11e14dd6c76ec12a6da93f10a49fc0c8
Alim Nastaev <a.nastaev@gmail.com>
2020-09-02 20:33:20 -0400

    refactoring using function heads and pattern matching

--- lib/1_csv_quiz.ex
+++ lib/1_csv_quiz.ex
@@ -1,26 +1,18 @@
 defmodule CsvQuiz do
-  @moduledoc """
-  Documentation for CsvQuiz.
-  """
+  def csv_quiz(path), do: read_file(path) |> implemenation()
 
-  def csv_quiz(path) do
-    # case statement to handle error if something wrong
-    # with csv file or the path
-    case File.read(path) do
-      {:error, reason} ->
-        IO.puts("#{:file.format_error(reason)}")
+  defp read_file(path), do: File.read(path)
 
-      {:ok, file} ->
-        [_, result] =
-          parser(file)
-          |> Enum.reduce([0, 0], fn array, [iteration, acc] -> solution(array, iteration, acc) end)
+  defp implemenation({:ok, file}) do
+    [_, result] =
+      parser(file)
+      |> Enum.reduce([0, 0], fn array, [iteration, acc] -> solution(array, iteration, acc) end)
 
-        IO.puts("\nYou scored #{result} of #{length(parser(file))}.")
-    end
+    IO.puts("\nYou scored #{result} of #{length(parser(file))}.")
   end
 
-  # function to parse a csv file and shape it to
-  # to iterate: [[problem, answer]]
+  defp implemenation({:error, reason}), do: IO.puts("#{:file.format_error(reason)}")
+
   defp parser(file) do
     file
     |> String.split("\n", trim: true)
@@ -31,16 +23,15 @@ defmodule CsvQuiz do
     num = iteration + 1
     input = IO.gets("Problem ##{num}: #{question} = ") |> String.trim()
 
-    result =
-      if input == answer do
-        acc + 1
-      else
-        acc
-      end
+    result = shaping_result(input, answer, acc)
 
     [num, result]
   end
+
+  defp shaping_result(input, answer, acc) when input == answer, do: acc + 1
+  defp shaping_result(_input, _answer, acc), do: acc
 end
+
 
 path = System.argv()
 
